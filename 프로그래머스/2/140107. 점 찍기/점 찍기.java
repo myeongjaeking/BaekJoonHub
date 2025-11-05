dd9a39e42283c4c8dd584985401561adfdaea4ad
myeongjaeking <112608799+myeongjaeking@users.noreply.github.com>
2025-11-05 16:40:11 +0900

    [level 2] Title: 점 찍기, Time: 17.64 ms, Memory: 87.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+class Solution {
+    public long solution(int k, int d) {
+        long answer = 0;
+        
+        for(int i =0; i<=d; i+=k) {
+            long max = (long) Math.sqrt((long)d * d - (long)i * i);
+            answer += (max / k) + 1;
+        }
+        
+        return answer;
+    }
+    
+}
